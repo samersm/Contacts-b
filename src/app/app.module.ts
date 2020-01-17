a96b5d466c2b6c36398da8d9343c1fca222c2836
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-01-17 17:55:32 +0300

    Revert All Previus Commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { AddContactComponent } from './components/add-contact/add-contact.component';
-import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
-import { EditContactComponent } from './components/edit-contact/edit-contact.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactsComponent,
-    AboutComponent,
-    AddContactComponent,
-    ContactDetailsComponent,
-    EditContactComponent,
-    NavbarComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
--- src/app/models/Contact.ts
+++
@@ -1,6 +0,0 @@
-export class Contact {
-    id: number;
-    name: string;
-    email: string;
-    mobile: number;
-}
